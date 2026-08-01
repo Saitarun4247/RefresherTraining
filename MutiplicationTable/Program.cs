dd3271e0b93729909a59029deeaad91973eb63e5
Sai Tharun <117085454+Saitarun4247@users.noreply.github.com>
2026-08-01 14:58:30 +0530

    Add files via upload

---
+++ MutiplicationTable/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class Solution
+{
+    public int[] MultiplicationTable(int n, int upto)
+    {
+        int[] row = new int[upto];
+
+        for (int i = 1; i <= upto; i++)
+        {
+            row[i - 1] = n * i;
+        }
+
+        return row;
+    }
+
+    public static void Main()
+    {
+        Solution s = new Solution();
+
+        int[] result = s.MultiplicationTable(3, 5);
+
+        Console.WriteLine(string.Join(", ", result));
+    }
+}

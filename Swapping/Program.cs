bb2251bfc9bd6a50b1771bdff63b9547990f4b61
Sai Tharun <117085454+Saitarun4247@users.noreply.github.com>
2026-08-08 10:21:44 +0530

    Add files via upload

---
+++ Swapping/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        int a = 10;
+        int b = 20;
+
+        Console.WriteLine($"Before swap: a = {a}, b = {b}");
+
+        SwapUsingRef(ref a, ref b);
+
+        Console.WriteLine($"After ref swap: a = {a}, b = {b}");
+
+        int x;
+        int y;
+
+        SwapUsingOut(a, b, out x, out y);
+
+        Console.WriteLine($"After out swap: a = {x}, b = {y}");
+    }
+
+    public static void SwapUsingRef(ref int a, ref int b)
+    {
+        a = a + b;
+        b = a - b;
+        a = a - b;
+    }
+
+    public static void SwapUsingOut(
+        int a,
+        int b,
+        out int x,
+        out int y)
+    {
+        x = a + b;
+        y = x - b;
+        x = x - y;
+    }
+}
